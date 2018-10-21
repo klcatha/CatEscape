81a70fd87388eb5b11c274051e6a1709e2aa0e56
klcatha <klcatha0518@gmail.com>
2018-10-22 07:47:19 +0900

    arrow generate span

--- Assets/ArrowGenerator.cs
+++ Assets/ArrowGenerator.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class ArrowGenerator : MonoBehaviour {
 
+	public GameObject arrowPrefab;
+	float span = 1.0f;
+	float delte = 0.0f;
+
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,14 @@ public class ArrowGenerator : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		this.delte += Time.deltaTime;
+		if(this.delte > this.span)
+		{
+			this.delte = 0.0f;
+			GameObject go = Instantiate(arrowPrefab) as GameObject;
+			int px = Random.Range(-6, 7);
+			go.transform.position = new Vector3(px, 7, 0);
+		}
 		
 	}
 }
