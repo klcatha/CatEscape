1cfb756f652bb2b06295474a3e665c0e7823a824
klcatha <klcatha0518@gmail.com>
2018-10-22 07:34:03 +0900

    hit judge

--- Assets/ArrorwController.cs
+++ Assets/ArrorwController.cs
@@ -4,9 +4,11 @@ using UnityEngine;
 
 public class ArrorwController : MonoBehaviour {
 
+	GameObject player;
+
 	// Use this for initialization
 	void Start () {
-		
+		this.player = GameObject.Find("player");
 	}
 	
 	// Update is called once per frame
@@ -19,6 +21,19 @@ public class ArrorwController : MonoBehaviour {
 		{
 			Destroy(gameObject);
 		}
-		
+
+		// あたり判定
+		Vector2 p1 = transform.position;				// 矢の中心座標
+		Vector2 p2 = this.player.transform.position;    // プレイヤの中心座標
+		Vector2 dir = p1 - p2;
+		float d = dir.magnitude;
+		float r1 = 0.5f;		//矢の半径
+		float r2 = 1.0f;		//プレイヤの半径
+
+		if(d < r1 + r2)
+		{
+			// 衝突した矢を消す
+			Destroy(gameObject);
+		}
 	}
 }
